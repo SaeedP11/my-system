ce1d2dfec07d34958e4055f650f24c91cbe3fe98
saeedp11 <saeed_abdi.p11@outlook.com>
2024-04-26 13:26:06 +0330

    wip: working on setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@
 ./yay_install.sh
 
 yay -S --needed - < packages.txt
+# Change shell to zsh
+chsh --shell /usr/bin/zsh
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone git@github.com:zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/plugins
+# Install nvm and pnpm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -fsSL https://get.pnpm.io/install.sh | sh -
